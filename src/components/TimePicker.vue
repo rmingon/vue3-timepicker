e858ccbb2dd843517cbb5a64cd5ae20018b6872d
rmingon <ronanmingon69550@gmail.com>
2023-03-16 21:04:43 +0100

    add emit + type

--- src/components/TimePicker.vue
+++ src/components/TimePicker.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex flex-col w-32 h-64 overflow-y-scroll block timepicker shadow-xl">
-    <input class="border p-1 m-2" type="text" v-model="search">
-    <div class="cursor-pointer text-xl hover:bg-gray-200 pl-2" v-for="time in times" value="">{{ time }}</div>
+  <div class="flex flex-col w-32 h-64 overflow-y-scroll block timepicker shadow-xl space-y-1">
+    <input class="border p-1 mx-2 mt-1" type="text" v-model="search">
+    <div class="cursor-pointer text-xl text-cyan-800 hover:bg-cyan-100 pl-2" @click="setHour(time)" v-for="time in times" value="">{{ time }}</div>
   </div>
 </template>
 
 <script setup lang="ts">
-import {computed, onMounted, ref} from "vue";
+  import {computed, onMounted, ref} from "vue";
 
   const minutes = ref([])
 
@@ -16,11 +16,24 @@ import {computed, onMounted, ref} from "vue";
     step: number
   }
 
+  interface Time {
+    hour: number,
+    minute: number
+  }
+
   const times = computed(() => minutes.value.filter(el => el.replaceAll(' ', '').includes(search.value.replaceAll(' ', ''))))
 
   const props = withDefaults(defineProps<Props>(), {
     step: 15
   })
+
+  const emits = defineEmits<{
+    (e: 'selected', time: Time)
+  }>()
+
+  const setHour = (time: Time) => {
+    emits('selected', time)
+  }
 
   onMounted(() => {
     for(let hour = 0; hour <= 23; hour++) {
@@ -34,7 +47,7 @@ import {computed, onMounted, ref} from "vue";
 <style scoped >
 
 .timepicker {
-  font-family: 'Tilt Neon', cursive;
+  font-family: 'Montserrat', cursive;
 }
 
 .scrollbar::-webkit-scrollbar {
