efa961c36ba8be42dade3221590d6b5bfd00d8bd
rmingon <ronanmingon69550@gmail.com>
2023-03-16 09:22:08 +0100

    add tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 
+  import TimePicker from './components/TimePicker.vue'
 </script>
 
 <template>
-
+  <TimePicker/>
 </template>
---
+++ src/components/TimePicker.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <select id=""></select>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps<{
+
+  }>()
+</script>
